a9d8f4db04dcbab60d1eec6c40debd68201b739e
Aditya Gandhi <adityagandhi176@gmail.com>
2024-01-14 10:53:14 -0500

    build fix 2

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Wrapper from "../Wrapper.js";
 // import Particle from "../Particle.tsx";
