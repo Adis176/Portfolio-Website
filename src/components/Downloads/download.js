d9aeb3aa0217355116648a9cfe5ac1aa1379ae77
Aditya Gandhi <adityagandhi176@gmail.com>
2025-02-21 19:21:50 -0500

    code splitting for resume

--- src/components/Downloads/download.js
+++ src/components/Downloads/download.js
@@ -1,19 +1,20 @@
-// import Resume from "../../Assets/Pdfs/Aditya_Gandhi_Resume.pdf";
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import Wrapper from "../Wrapper.js";
-// import Particle from "../Particle.tsx";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { AiOutlineArrowDown } from "react-icons/ai";
 // import Resume from "../../Assets/Images/Aditya_Resume.png";
 import AdityaResume from "../../Assets/Images/Aditya_FrontEnd.jpg";
 import "./download.css";
-// import Wrapper from "../Wrapper.js";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-// convert pdf to image - https://tools.pdf24.org/en/
-export default function Download(){
-    const Resume2 = require("../../Assets/Pdfs/Aditya_Resume_FrontEnd.pdf");
 
-    // const pdfUrl = "../../Assets/Pdfs/Aditya_Gandhi_Resume.pdf";
+export default function Download(){
+    const Fallback = () => {
+        return (
+            <div style={{backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', height: '6em', borderRadius: '1em', border: '0.4px solid rgba(66, 66, 66, 0.8)', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'rgba(200, 200, 200, 0.6)'}}>
+                <p style={{margin: 'auto'}}>Resume is Missing</p> 
+            </div>
+        );
+    }
+    const Resume2 = require("../../Assets/Pdfs/Aditya_Resume_FrontEnd.pdf");
     const onButtonClick = () => {
         const pdfUrl2 = Resume2;
         const link = document.createElement("a");
@@ -33,7 +34,9 @@ export default function Download(){
             </Row>
             <Row xs={12} style={{justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
                 <Col xs={12} md={10} lg={9} style={{justifyContent: 'center', alignItems: 'center', display: 'flex', marginTop: '2.5em'}}>
-                    <img src={AdityaResume} alt="Aditya_Resume" className="download-resume-img"/>
+                    <Suspense fallback={<Fallback />}>
+                        <img src={AdityaResume} alt="Aditya_Resume" className="download-resume-img"/>
+                    </Suspense>
                 </Col>
             </Row>
         </Container>
